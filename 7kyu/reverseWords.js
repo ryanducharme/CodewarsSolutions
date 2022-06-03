9369cd4ed5f229933fe5fec67ff7d43083518362
Ryan DuCharme <ryan.ducharme77@gmail.com>
2022-06-03 13:47:58 -0400

    Create reverseWords.js

---
+++ 7kyu/reverseWords.js
@@ -0,0 +1,10 @@
+function reverseWords(str) {
+  let origWords = str.split(' ');
+  let newWords = [];
+  origWords.forEach(element => {
+    newWords.push(element.split('').reverse().join(''));
+  });
+  return newWords.join(' ');
+}
+
+reverseWords('The quick brown fox jumps over the lazy dog.');
